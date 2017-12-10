87bb4d05b0ad17e66e772462b4284a05b532be95
Dan Singer <dsinger.sax@gmail.com>
2017-12-10 02:23:07 -0500

    Complete pickups, fix text selection bug

--- project1/project3/js/main.js
+++ project1/project3/js/main.js
@@ -19,11 +19,18 @@ const gameManager = {
         this.app = new PIXI.Application(gameContainer.clientWidth, gameContainer.clientHeight);
         gameContainer.appendChild(this.app.view);
 
+        this.app.view.onclick = (e) => {
+            console.log("clicked canvas");
+            e.preventDefault();
+        }
+
         PIXI.loader
             .add("media/player.png")
             .add("media/player-hit.png")
             .add("media/enemy.png")
             .add("media/bullet.png")
+            .add("media/pickup-health.png")
+            .add("media/pickup-score.png")
             .load(()=>{
                 this.assetsLoaded();
             });
@@ -36,11 +43,13 @@ const gameManager = {
 
         let player = new Player("player", this.app);
         player.position = {x: this.app.screen.width/2, y: this.app.screen.height/2};
-
         enemy = new WanderFireEnemy("enemy", this.app, player, {x: this.app.screen.width, y: this.app.screen.height/2});
+
+        let p1 = new HealthPickup("health", this.app, {x: 50, y:50});
         let mainScene = new PIXI.Container();
         mainScene.addChild(player);
         mainScene.addChild(enemy);
+        mainScene.addChild(p1);
         
 
         this.app.stage.addChild(mainScene);
@@ -57,3 +66,4 @@ const gameManager = {
 window.onload = ()=>{
     gameManager.windowLoaded();
 };
+
--- project1/project3/js/pickups.js
+++ project1/project3/js/pickups.js
@@ -1,17 +1,36 @@
 class Pickup extends GameObject{
     constructor(name, app, position=null){
         super(name, app, position);
+        this.worth = 1;
         this.attachCollider();
-    }
-}
-class HealthPickup extends Pickup{
-    constructor(name, app, position=null){
-        super(name, app, position);
     }
 
     onCollisionBegin(other){
         if (other.gameObject instanceof Player){
-
+            this.onPickedUp(other.gameObject);
+            this.destroy();
         }
+    }
+
+    onPickedUp(player){ }
+}
+class HealthPickup extends Pickup{
+    constructor(name, app, position=null){
+        super(name, app, position);
+        this.addSprite("media/pickup-health.png");
+    }
+
+    onPickedUp(player){
+        player.adjustHealth(this.worth);
+    }
+}
+class ScorePickup extends Pickup{
+    constructor(name, app, position=null){
+        super(name, app, position);
+        this.addSprite("media/pickup-score.png");        
+    }
+
+    onPickedUp(player){
+        gameManager.score += this.worth;
     }
 }
